13c7f17de478df2ae2309e0620d38d122a4bd65e
Lalon Sander <lalon.sander@gmail.com>
2025-11-26 14:33:53 +0100

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_26_092348) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_26_125730) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_26_092348) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
-    t.string "content"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "restaurant_id", null: false
