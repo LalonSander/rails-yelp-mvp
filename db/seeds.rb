eb68626a50be42daf73fe86a5cc5c16aef4f8393
Lalon Sander <lalon.sander@gmail.com>
2025-11-26 11:17:09 +0100

    routes done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+10.times do
+  Restaurant.create(name: "#{Faker::Restaurant.name}",
+              category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+              address: "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.country}")
+end
